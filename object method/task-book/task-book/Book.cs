cce86f87cf5928f0957201c5e481fe3e93524e7c
ALEKSI101 <Aleksi198_@hotmail.com>
2018-01-12 09:36:42 +0200

    task-book completed

---
+++ object method/task-book/task-book/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_book
+{
+    class Book
+    {
+        //Fields
+        public string Title;
+        public string Author;
+        public int Id;
+        public int Price;  
+        
+        //Constructor
+        public Book()
+        {
+            Title = "unknown";
+            Author = "unknown";
+            Id = 0;
+            Price = 0;
+        }
+        public Book(string title, string author, int id, int price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Id = id;
+            this.Price = price;
+        }
+
+        //Methods
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi:{Title}\n" +
+                              $"Kirjoittajan nimi:{Author}\n" +
+                              $"Id-numero:{Id}\n" +
+                              $"Kappalehinta:{Price}");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return $"{this.Title} on kalliimpi kuin {book.Title} ";
+            else
+                return $"{book.Title} on kalliimpi kuin {this.Title} ";
+
+        }
+    }
+}
---
+++ object method/task-book/task-book/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookRaamattu = new Book();
+            bookRaamattu = new Book("Raamattu", "Jeesus", 654612, 10);
+            bookRaamattu.PrintBookInfo();
+
+            Book bookTuntematonSotilas = new Book();
+            bookTuntematonSotilas = new Book("Tuntematon sotilas", "Väinö Linna", 210, 22);
+            bookTuntematonSotilas.PrintBookInfo();
+            Console.WriteLine ($"{bookRaamattu.CompareBook(bookTuntematonSotilas)}");
+           
+
+                Console.ReadKey();
+
+        }
+    }
+}
