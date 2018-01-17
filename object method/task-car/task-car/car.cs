fe87d083998b30960ba0a64192828c394fa94646
ALEKSI101 <Aleksi198_@hotmail.com>
2018-01-17 20:30:58 +0200

    initialized files

---
+++ object method/task-car/task-car/car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_car
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public int Speed;
+    }
+    // Constructor
+    public Car()
+    {
+        
+    }
+
+    // Methods
+    public void AskData()
+    {
+
+
+    }
+    public void ShowCarInfo()
+    {
+
+    }
+}
