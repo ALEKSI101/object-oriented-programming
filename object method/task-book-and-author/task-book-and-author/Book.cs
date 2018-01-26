ebc18f775bf9cfacd96a5b4139fca3e23ef41b93
ALEKSI101 <Aleksi198_@hotmail.com>
2018-01-26 10:48:17 +0200

    initialized files

---
+++ object method/task-book-and-author/task-book-and-author/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_book_and_author
+{
+    class Book
+    {
+        //Fields
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public double Price;
+        public static string Theme;
+    }
+
+    //Constructor
+    public Book()
+    {
+        Name = null;
+        Author = null;
+        Publisher = null;
+        Price = 0.00;
+        Theme = null;
+    }
+    public Book(string Name, string author, string publisher, double price, string theme)
+    {
+        name = name;
+        author = author;
+        publisher = publisher;
+        price = price;
+        theme = theme; 
+    }
+    
+    //Methods
+
+    public void 
+
+}
---
+++ object method/task-book-and-author/task-book-and-author/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_book_and_author
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
