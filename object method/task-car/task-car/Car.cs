363e071b2d9d29e55f9eefb3ac083720001821d6
ALEKSI101 <Aleksi198_@hotmail.com>
2018-01-26 08:53:21 +0200

    task-car completed

---
+++ object method/task-car/task-car/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_car
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public double Speed;
+
+        //Constructor
+               
+        public Car()
+        {
+            Brand = null;
+            Speed = 0;
+        }
+
+
+        //Methods
+        
+
+
+        public void AskData()
+        {
+
+            Console.WriteLine("Auton merkki: ");
+            Brand = Console.ReadLine();
+            Console.WriteLine("Auton nopeus: ");
+            Speed = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki: {Brand}\nNopeus:{Speed}\n");
+        }
+        public void Accelerate()
+        {
+            Speed = Speed + 10;
+        }
+        public void Accelerate(int moreSpeed)
+        {
+            if (moreSpeed > 0)
+            {
+                Speed = Speed + moreSpeed;
+                Console.WriteLine($"Auto kiihdyttää. Uusi nopeus on: {Speed}");
+            }
+            else
+            {
+                Console.WriteLine($"Negatiivinen muutos ei ole sallittu!");
+            }
+
+            
+                        
+
+        }
+        public void Brake()
+
+        {
+            Speed = Speed * 0.9;
+            Console.WriteLine($"Auto jarruttaa. Nopeus jarrutuksen jälkeen on: { Speed  }");
+        }
+    }
+}
---
+++ object method/task-car/task-car/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+
+            car1.AskData();
+            car1.ShowCarInfo();
+            car1.Accelerate();
+            car1.ShowCarInfo();
+            car1.Brake();
+            car1.ShowCarInfo();
+            
+
+            Car car2 = new Car();
+            car2.AskData();
+            car2.ShowCarInfo();           
+            car2.Accelerate();
+            car2.ShowCarInfo();
+            car2.Brake();
+            car2.ShowCarInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
