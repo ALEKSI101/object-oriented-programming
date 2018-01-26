d8cd7e161dd9f2141f2c44d6c1b4d8b76dc849bf
ALEKSI101 <Aleksi198_@hotmail.com>
2018-01-26 10:16:18 +0200

    Initialized files

---
+++ object method/task-company/task-company/Company.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_company
+{
+    class Company
+        //Fields
+        
+    {        
+        public string Title;
+        public string Address;
+        public string Phone;
+        public double Outcome;
+        public double Expense;
+    }
+
+    //Constructor
+    public Company()
+    {
+        Title = null;
+        Address = null;
+        Phone = null;
+        Outcome = 0.00;
+        Expense = 0.00;
+    }
+    
+    public Company(string title, string address, string phone, double outcome, double expense)
+    {
+        Title = title;
+        Address = address;
+        Phone = phone;
+        Outcome = outcome;
+        Expense = expense;
+    }
+    
+    //Copy Constructor
+    public Company(Company previousCompany)
+    {
+        Title = previousCompany.Title;
+        Address = previousCompany.Address;
+        Phone = previousCompany.Phone;
+        Outcome = previousCompany.Outcome;
+        Expense = previousCompany.Expense;
+
+    }
+
+    //Methods
+
+    public void ShowCompanyInfo()
+    {
+        Console.WriteLine($"Yrityksen nimi:{Title}");
+        Console.WriteLine($"Yrityksen osoite:{Address}");
+        Console.WriteLine($"Yrityksen puhelinnumero:{Phone}");
+        Console.WriteLine($"Yrityksen tulot:{Outcome}");
+        Console.WriteLine($"Yrityksen menot:{Expense}");
+    }
+
+    public void Profit()
+    {
+        Profit = ((Outcome - Expense) / Expense * 100);
+
+        if (Profit >= 300)
+        {
+            Console.WriteLine("Firmalla menee hyvin.");
+        }
+            
+        else if (Profit >= 200)
+        {
+            Console.WriteLine("Firmalla menee tyydyttävästi.");
+        }
+        else
+        {
+            Console.WriteLine("Firmalla menee kehnosti.");
+        }
+            
+        
+
+    }
+    
+}
---
+++ object method/task-company/task-company/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company newCompany = new Company();
+            newCompany.Title = "Sterling & Cooper";
+            newCompany.Address = "Majatalo 55";
+            newCompany.Phone = "0400962521";
+            newCompany.Outcome = 100.00;
+            newCompany.Expense = 50.00;
+
+            Console.ReadKey();
+            
+        }
+    }
+}
